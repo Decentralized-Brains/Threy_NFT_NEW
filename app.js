fe51a42cdbe836a98b7d084c1fdf590a1e64c67d
alfazsozib <alfazsozib@gmail.com>
2023-02-18 03:56:56 +0600

    word selection fixed

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.post("/set-data", async (req, res) => {
     if (!user) user = new User({ wallet })
 
     if (char && !user.char) user.char = char
-    if (word && !user.word) {
+    if (word) {
       const totalWordCount = await User.countDocuments({ word })
       if (totalWordCount == word.length) return res.status(500).json({ message: "This word is full! Try another" })
       user.word = word
