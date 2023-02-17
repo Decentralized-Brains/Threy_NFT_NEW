6966de42764b2277b363132cc90d9b630879329f
alfazsozib <alfazsozib@gmail.com>
2023-02-18 01:39:06 +0600

    Changes Done

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -19,7 +19,7 @@ function Main() {
       // Re-enable scrolling and set the scroll position back to where it was
       document.body.style.overflow = "";
       window.scrollTo(scrollLeft, scrollTop);
-    }, 15000);
+    }, 16000);
     window.onbeforeunload = function () {
       window.scrollTo(0, 0);
     }
